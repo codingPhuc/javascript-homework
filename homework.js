c869c900938b89cb7a1f8cc583fc620b759b3aaa
Nguyen Hoang Phuc <phucnguyenhoang3839@gmail.com>
2024-06-26 19:55:37 +0700

    Main | default create

---
+++ homework.js
@@ -0,0 +1,14 @@
+
+
+var NumberValue = 42;         
+var StringValue= "Hello";   
+var BooleanValue = true;     
+var UndefinedValue;         
+var NullValue 
+
+
+console.log(typeof myNumber);    
+console.log(typeof myString);
+console.log(typeof myBoolean);   
+console.log(typeof myUndefined); 
+console.log(typeof myNull);    
